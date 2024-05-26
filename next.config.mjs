5239c3a5f2100ccca1a46f7ec2c65574a7cb2c7e
sosso21 <sofianetop21.st@gmail.com>
2024-05-26 07:11:15 +0100

    :sparkles: (add): Translation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import dotenv from 'dotenv';
+
+if (process.env.APP_ENV === 'testing') {
+  // expecting to overwrite DATABASE_URL and other params
+  dotenv.config({ path: '.env.testing', override: true });
+} else if (process.env.APP_ENV === 'development') {
+  dotenv.config({ path: '.env', override: true });
+}
+const { DEFAULT_LANG } = process.env; // for some reason it has no issue reading the variables from here
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  env: { DEFAULT_LANG },
+};
 
 export default nextConfig;
