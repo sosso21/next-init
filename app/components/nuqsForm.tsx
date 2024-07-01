05384e9e237250a26b976d6548d9963866215a0b
sosso21 <sofianetop21.st@gmail.com>
2024-07-01 09:15:26 +0100

    :fire: (refact) version next auth

--- app/components/nuqsForm.tsx
+++ app/components/nuqsForm.tsx
@@ -9,14 +9,14 @@ export const Nuqs = () => {
       <button onClick={() => setCount(0)}>Reset</button>
       {/* handling null values in setCount is annoying: */}
       <button
-        className="bg-primary rounded text-primary-foreground"
-        onClick={() => setCount((c) => c + 1)}
+        className="rounded bg-primary text-primary-foreground"
+        onClick={() => setCount((c) => (c ?? 0) + 1)}
       >
         +
       </button>
       <button
-        className="bg-secondary rounded text-secondary-foreground"
-        onClick={() => setCount((c) => c - 1)}
+        className="rounded bg-secondary text-secondary-foreground"
+        onClick={() => setCount((c) => (c ?? 0) - 1)}
       >
         -
       </button>
